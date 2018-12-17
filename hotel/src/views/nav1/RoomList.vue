b0263b09ecfb306cfb0694f106146fca3390d754
zhouwei199388 <15090824065@163.com>
2018-12-17 22:26:00 +0800

    文件上传测试

--- hotel/src/views/nav1/RoomList.vue
+++ hotel/src/views/nav1/RoomList.vue
@@ -83,6 +83,18 @@
                 <el-form-item label="图片">
                     <el-input v-model="addForm.image"></el-input>
                 </el-form-item>
+                <el-upload
+                        class="upload-demo"
+                        :on-preview="handlePreview"
+                        :on-remove="handleRemove"
+                        :before-remove="beforeRemove"
+                        multiple
+                        :limit="3"
+                        :on-exceed="handleExceed"
+                        :file-list="fileList">
+                    <el-button size="small" type="primary">点击上传</el-button>
+                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                </el-upload>
                 <el-form-item label="窗户">
                     <el-radio-group v-model="addForm.window">
                         <el-radio :label="0">有窗</el-radio>
@@ -111,6 +123,7 @@
     export default {
         data() {
             return {
+                fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
                 filters: {
                     name: ''
                 },
@@ -156,6 +169,18 @@
             }
         },
         methods: {
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+            },
+            handleExceed(files, fileList) {
+                this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+            },
+            beforeRemove(file, fileList) {
+                return this.$confirm(`确定移除 ${ file.name }？`);
+            },
             // //性别显示转换
             formatWindow: function (row, column) {
             	return row.window === 0 ? '有窗':'无窗';
